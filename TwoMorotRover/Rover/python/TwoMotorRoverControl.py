05fa2a8891e6b66eaabd92a7bd3c2832d0badf95
vipulasrilanka <vipulasrilanka@yahoo.com>
2020-05-30 19:07:23 +0530

    Add comments to the program

--- TwoMorotRover/Rover/python/TwoMotorRoverControl.py
+++ TwoMorotRover/Rover/python/TwoMotorRoverControl.py
@@ -3,7 +3,7 @@ from gpiozero import PWMLED
 from gpiozero import LED
 from time import sleep
 
-#pin assignment
+#GPIT pin assignment
 C_Turn_L = 23
 C_Turn_R = 22
 C_Turn_S = 26
@@ -11,15 +11,18 @@ C_Run_F = 4
 C_Run_B = 17
 C_Run_S = 27
  
-UDP_IP = "192.168.1.200"
+ #We are using simple UDP packets to control the morots 
+ #We have the Port hard coded at the momenrt. 
+UDP_IP = "192.168.1.206"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+sock.bind((UDP_IP, UDP_PORT)) #bind the socket to IP and port
     
     
-    
+ #   Two_Motor_drive_Rover defines the two motor driver
+ #  It has to be initialised with the GPIO pins. Each instance can be given a name.
 class Two_Motor_drive_Rover:
     
     def __init__(self,name,G_Turn_L,G_Turn_R,G_Turn_S,G_Run_F,G_Run_B,G_Run_S):
@@ -31,19 +34,25 @@ class Two_Motor_drive_Rover:
         print ("FWRD",G_Run_F)
         print ("BKWD",G_Run_B)
         print ("SPED",G_Run_S)
+        # Creating objects for GPIO Control.
+        # We are using the gpiozero library for this. 
         self.Turn_L = LED(G_Turn_L)
         self.Turn_R = LED(G_Turn_R)
         self.Run_F  = LED(G_Run_F)
         self.Run_B  = LED(G_Run_B)
         self.Turn_S = PWMLED(G_Turn_S)
         self.Run_S = PWMLED(G_Run_S)
-        self.BreakStatus = 'R'
-        self.GearPosition = 'N'
+        # Initialize local state variables
+        self.BreakStatus = 'R' # This keeps track of the Break state. (R = Releaeed, A = Active)
+        self.GearPosition = 'N' # This Keeps the Gear Level (N = Neutral, R = Reverse, D = Drive and Numbers can be used)
 
  
 # Turn function 
+# The implmentation is specific to the Chasis that this code runs
 
     def Turn(self, direction, angle):
+        # The current Chasis can not contrl the Angle so we take only the direction.
+        # once this function is called, the motors will be activated,.
         if direction == 'L':
             self.Turn_L.on()
             self.Turn_R.off()
@@ -52,54 +61,78 @@ class Two_Motor_drive_Rover:
             self.Turn_L.off()
             self.Turn_R.on()
             self.Turn_S.value = 1
-        else:
+        else: # The only option that is applicable for the current Chasis is "C" for centre. 
             self.Turn_L.off()
             self.Turn_R.off()
             self.Turn_S.value = 0
         print ("Turn",direction, angle)
             
 # Gear Shift Function 
+# The implmentation is specific to the Chasis 
+# Change the Gear posision. This will not drive motors.
                 
     def Gear(self,gear):
         if gear == 'N':
+            #Release motor Driver
             self.Run_F.off()
             self.Run_B.off()
             self.Run_S.value = 0
         elif gear == 'R':
+            #Enable Reverse (Run_B.on)
             self.Run_F.off()
             self.Run_B.on()
             self.Run_S.value = 0
-        else:
+        else: # this can be D, 1, 2, 3, ..etc
+            #Enable Drive  (Run_F.on)
             self.Run_F.on()
             self.Run_B.off()
             self.Run_S.value = 0
         self.GearPosition = gear
         print ("Drive",self.GearPosition)
             
-# Gear Accelerate function ( values : 0 - 100)
-    
+# Motor Accelerate function ( values : 0 - 100)
+# The current chasis has 6V motor and 8.4V battery pack. 
+# So keep the Speed below 80 for safety.
+# This method sets the Speed of the motor.    
+
     def Speed(self,speed):
         if self.GearPosition == 'N':
+            # If you are in Neutral, no action will be done. This Chasisi has the motor directly coupled to wheels.
             self.Run_S.value = 0
             print ("Neutral. Can not go.!")
         elif self.BreakStatus == 'A':
+            # If you are in Neutral, no action will be done. This Chasisi has the motor directly coupled to wheels.
             print ("Break ON. Can not go.!")
         else:
-            self.Run_S.value = speed/100
+            # The PWMLED function takes a float between 0 - 1 
+            # So we device the Speed value by 100 to get the correct value. 
+            # This will run the motor with given Speed.
+            if speed >80:
+                self.Run_S.value = 0.8 #Limit the max speed as we use 6V motor.  
+            else:
+                self.Run_S.value = speed/100
             print ("speed", speed)
 
+# Activate Breaks.
+# This implementation is specific to the Chasis 
+
     def Break(self, activate):
-        if activate == 'A':#Apply break
+        if activate == 'A':
+          #Apply Break (Enable Both F and B in this controller, and eneble full speed)     
           self.Run_F.on()
           self.Run_B.on()
           self.Run_S.value = 1
           print ("Break on")
-        elif activate == 'R':#Release Break
+        elif activate == 'R':
+          #Release Break. Here we put the Chasis in to the previous Gear level that's stored in GearPosition.
           print ("Break Release")
           self.Gear(self.GearPosition) #back to active Gear
         else:
           print ("error")
+        #Store the current Brek Status,
         self.BreakStatus = activate 
+
+#Dump the GPIO map for debugging. 
 
     def DumpGpioMap(self):
         print(self.Turn_L) 
@@ -109,8 +142,9 @@ class Two_Motor_drive_Rover:
         print(self.Turn_S) 
         print(self.Run_S) 
             
-#Start maim()
+#Start main()
 
+#Construct the Rover Named Kevin
 Rover = Two_Motor_drive_Rover ("Kevin",C_Turn_L,C_Turn_R,C_Turn_S,C_Run_F,C_Run_B,C_Run_S)
 
 # Rover.Turn('L',30)
@@ -148,23 +182,32 @@ Rover = Two_Motor_drive_Rover ("Kevin",C_Turn_L,C_Turn_R,C_Turn_S,C_Run_F,C_Run_
 # Rover.Speed(0)
 
 print ("start")
+# Main Loop.
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    #data will contain the data and addr will have the sender address
+    #We can filter the host here but not implememted yet,
     print ("received message:", data)
+
+    #Turning Functions
     if data == b'LEFT':
         Rover.Turn('L',30) #Turn Left
     elif data == b'RIGT':
         Rover.Turn('R',30) #Turn Right
     elif data == b'CNTR':
         Rover.Turn('C',0)  #Centre
-        
+
+     #Driving Functions   
     elif data == b'FWRD':
-        Rover.Gear('D')   #Drive (Gear Change)
+        Rover.Gear('D')   #Drive (Gear Change only)
     elif data == b'REVR':
         Rover.Gear('R')   #Drive back (Gear Change only)
-    elif (data[0] == 83 and data [1] == 80): #if start with SP
+    #Now check for Speed setting
+    elif (data[0] == 83 and data [1] == 80): #if the packet is of SPnn,  S = (ASCII 82) P  =(ASCII P)
         if (Rover.GearPosition == 'D' or Rover.GearPosition == 'R'):
-            Rover.Speed((10*(data[2]-48))+ (data[3] - 48)) #Set Speed
+            Rover.Speed((10*(data[2]-48))+ (data[3] - 48)) #Set Speed 0 = (ASCII 48)
+        else:
+            print ("Invalid Speed. Not in a Driving gear")
     elif data == b'BREK':
         Rover.Break('A')  #Break Apply
     elif data == b'NBRK':
@@ -172,7 +215,9 @@ while True:
     elif data == b'NUTR':
         Rover.Gear('N')   #Neutral Gear
         Rover.Speed(0)
-    elif data == b'TERM':
+        
+    # Remote control functios for main loop
+    elif data == b'TERM':# This terminates the loop and exit the program
         Rover.Speed(0)
         Rover.Turn('C',0)
         Rover.Gear('N')   #Terminate Loop
